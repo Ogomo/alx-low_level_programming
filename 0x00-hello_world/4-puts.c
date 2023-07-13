086df366e8171393db797895760ae46a10e63288
Ogomo <ogomolinsen0792@gmail.com>
2023-07-13 12:05:24 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a line using the function puts
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
