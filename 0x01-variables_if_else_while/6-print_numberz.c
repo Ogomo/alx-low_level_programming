e7af8cab5d75306991f7a373f226eca5c64a458f
Ogomo <ogomolinsen0792@gmail.com>
2023-07-14 14:17:32 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print single digit numbers
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while
+		(x < 10)
+		{
+			putchar(x + '0');
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
