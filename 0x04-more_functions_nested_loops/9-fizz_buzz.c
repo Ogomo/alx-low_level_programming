fff12cf92f2ff951281e161e42d68854c4559c06
Ogomo <ogomolinsen0792@gmail.com>
2023-07-21 13:49:31 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,26 @@ int main(void)
 
 	for (i = 1; 1 <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
+		{
 			printf("Fizz");
-		else if (i % 5 == 0)
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
 			printf("Buzz");
-		else if (i % 15 == 0)
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else
-			printf("%i", i);
-		if (i < 100)
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
 			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
